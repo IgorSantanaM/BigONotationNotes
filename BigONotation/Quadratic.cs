f3bf7db736f9fab38b0a69b0afd5c1ba0588e138
IgorSantanaM <igorsantanamedeiros17@gmail.com>
2024-07-09 15:19:35 -0400

    Initial commit

---
+++ BigONotation/Quadratic.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BigONotation
+{
+    public class Quadratic
+    {
+        public void log(int[] nums)
+        {
+            //Quadratic 
+            //O(n^3)
+
+            foreach (int first in nums)//O(n)
+            {
+                foreach (int second in nums)//O(n)
+                    foreach (int third in nums)
+                    Console.WriteLine(first + " ," + second + " ," + third);
+            }
+        }
+    }
+}
